e11b960ed57c3668af8daf466344eb437d4abc1c
domenicobacoli <domenico.bacoli89@gmail.com>
2023-05-11 19:25:47 +0200

    feat: print on page trains data without style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts/main')
 
 @section('content')
-    Ciao
+<div class="card">
+    @foreach ($trains as $train)
+        
+    <div class="card-details">
+      <h5 class="card-title">Azienda: {{$train->azienda}}</h5>
+      <h6 class="card-subtitle">Stazione di partenza: {{$train->stazione_partenza}}</h6>
+      <div class="card-text">Stazione di arrivo: {{$train->stazione_arrivo}}</div>
+      <div class="card-text">Data di partenza: {{$train->data_partenza}}</div>
+      <div class="card-text">Orario di partenza: {{$train->orario_partenza}}</div>
+      <div class="card-text">Orario di arrivo: {{$train->orario_arrivo}}</div>
+      <div class="card-text">Codice Treno: {{$train->codice_treno}}</div>
+      <div class="card-text">In orario: {{$train->in_orario ? 'Si' : 'No'}}</div>
+      <div class="card-text">Cancellato: {{$train->cancellato ? 'Si' : 'No'}}</div>
+
+    </div>
+    @endforeach
+</div>
 @endsection
